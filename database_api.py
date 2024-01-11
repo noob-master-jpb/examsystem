e8cb679b899e69bbee54cdec9ea7ad0c977c234d
Jyotipriya <b8777051595@gmail.com>
2024-01-11 12:27:09 +0530

    .

--- database_api.py
+++ database_api.py
@@ -20,15 +20,27 @@ class Login(Database):
         self.control.execute(f"select* from login where username = '{user}';")
         return self.control.fetchall()
     
-class Personal(Database):
+    
+class Reader(Database):
+    def table_reader_all(self,table):
+        self.control.execute(f"select * from {table} ")
+        return self.control.fetchall()
+    
+    def table_reader_specific(self,table,data,by):
+        self.control.execute(f"select * from {table} where {by} = '{data}';")
+        return self.control.fetchall()
+    
+    def table_reader_col(self,table,data,by,row):#error can orrcur in rows
+        self.control.execute(f"select ({row}) from {table} where {by} = '{data}'; ")
+    
     def personal_data_all(self):
-        self.control.execute("select * from personal data")
+        self.control.execute("select * from personal;")
         return self.control.fetchall()
     
     def personal_data_user(self,user,by="username"):
         self.control.execute(f"""select * from personal where {by} = '{user}';""")
         return self.control.fetchall()
-class Results(Database):
+    
     def results_all(self):
         self.control.execute("""
                              select * from results;
@@ -40,3 +52,6 @@ class Results(Database):
                              select * from results where {by} = '{user}';
                              """)
         return self.control.fetchall()
+    
+
+    
